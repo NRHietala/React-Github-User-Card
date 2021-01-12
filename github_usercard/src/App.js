d9212c07d92208ab85622540b0cc3c821fc9093c
Nathan Hietala <Nrhietala@gmail.com>
2021-01-12 15:57:45 -0600

    MVP finished, doing styling but exceeded API limit

--- github_usercard/src/App.js
+++ github_usercard/src/App.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
-// import UserCard from './UserCard';
-import './App.css';
 
+
+const Wrapper = styled.div `
+display:flex;
+flex-flow:column nowrap;
+justify-content:center;
+align-items:center;
+height: 100vh;
+background: tan;
+
+h1 {
+  text-decoration:underline;
+  font-size: 40px;
+}
+
+.formInput {
+  padding: 10px 5px;
+  margin: 10px;
+  text-align:center;
+}
+
+.searchBtn {
+  padding: 10px;
+  color:red;
+}
+
+.userInfo {
+  display:flex;
+  flex-flow:column nowrap;
+  align-items:center;
+  justify-content:center;
+  background:whitesmoke;
+  padding: 10px;
+  font-size: 25px;
+}
+
+.avatar {
+    border-radius: 25px;
+  }
+`
 class App extends React.Component {
   state =  {
     userData: [],
@@ -15,6 +53,7 @@ class App extends React.Component {
     .get(`https://api.github.com/users/NRHietala`)
     .then(res => {
       const newData = res.data
+      console.log(newData);
       this.setState({
         userData: newData
       })
@@ -35,36 +74,40 @@ class App extends React.Component {
     event.preventDefault();
     axios.get(`https://api.github.com/users/${this.state.searchValue}`)
     .then(res => {
-      console.log(res.data)
+      const searchData = res.data
+      console.log(searchData);
       this.setState({
-        userData: res.data
+        userData: searchData
       })
-      .catch(err => {
-        console.log(err)
-      })
+    })
+    .catch(err => {
+      console.log(err)
     })
   }
 
   render() {
     return (
-      <div>
+      <Wrapper>
        <h1>Search for a GitHub User!</h1>
        <form>
          <input
          type="text"
+         className="formInput"
          onChange={this.handleChange}
          placeholder='Search for User'
          />
-         <button onClick={this.handleClick}>Search</button>
+         <button className ="searchBtn"onClick={this.handleClick}>Search</button>
        </form>
         <div className="cardContainer">
-          <img src={this.state.userData.avatar_url}/>
-          <h2>{this.state.userData.name}</h2>
-          <p>{this.state.userData.bio}</p>
-          <p>Followers: {this.state.userData.followers}</p>
-          <p>Following: {this.state.userData.following}</p>
+          <img className="avatar" src={this.state.userData.avatar_url}/>
+          <div className="userInfo">
+            <h2>{this.state.userData.name}</h2>
+            <p>{this.state.userData.bio}</p>
+            <p>Followers: {this.state.userData.followers}</p>
+            <p>Following: {this.state.userData.following}</p>
+          </div>
         </div>
-      </div>
+      </Wrapper>
     )
   }
 }
